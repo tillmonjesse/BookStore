eb1058ea8a3a21cf6a5ba6c7e0696798a0a62658
Adam Kaplan <euplonka@gmail.com>
2020-03-01 21:14:07 -0800

    working test

--- test/bookstore/BookShelf.test.js
+++ test/bookstore/BookShelf.test.js
@@ -1,4 +1,4 @@
-import {bookShelf,create,read,change,remove} from '../../src/bookstore/BookShelf.js';
+import {read} from '../../src/bookstore/BookShelf.js';
 
 it('read displays book for the given id', () => {
   const book = read('c7aa70e7-edfe-4593-999e-a6656d744146');
